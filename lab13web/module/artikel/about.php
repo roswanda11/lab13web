a6579862677b8d9c30c0289e24e90ad5d226e3bf
roswanda11 <roswanda2004@gmail.com>
2023-12-30 13:15:57 +0700

    upload file

---
+++ lab13web/module/artikel/about.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+
+</head>
+
+<body>
+        <?php require('../../template/header.php'); ?>
+        <section id="about" class="about">
+            <h2>About</h2>
+            <div class="jumbotron">
+                <div class="about-img">
+                    <img src="DSC08536.JPG">
+                </div>
+                <div class="content">
+                    <h3>Roswanda Nuraini</h3>
+                    <p>Hallo, Saya adalah Mahasiswa Semester 3 Jurusan Teknik Informatika 
+                 Universitas Pelita Bangsa. Aktif dalam keanggotaan organisasi kampus (HIMA).
+                 Memiliki ketertarikan di bidang <i>Web Developer</i> dan <i>Data Analyst.</i></p>
+                </div>
+        </section>
+        <?php require('../../template/footer.php'); ?>
+    </div>
+</body>
+
+</html>
---
+++ lab13web/template/footer.php
@@ -0,0 +1,3 @@
+<footer>
+    <p>&copy; 2023, Roswanda Nuraini, Universitas Pelita Bangsa</p>
+</footer>
